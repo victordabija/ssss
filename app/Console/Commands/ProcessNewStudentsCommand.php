a03081c2e84e9ed5a83625a27485ae553a23bf05
Dabija Victor <victorsvlogs31@gmail.com>
2025-05-07 12:29:40 +0300

    feat: ProcessNewStudentsCommand

---
+++ app/Console/Commands/ProcessNewStudentsCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\ParseStudentJob;
+use App\Jobs\ProcessStudentContentJob;
+use App\Models\Student;
+use Illuminate\Console\Command;
+
+class ProcessNewStudentsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:process-new-students';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Student::query()->whereNull('content')->get()->each(function (Student $student) {
+            dispatch(new ParseStudentJob($student));
+        });
+    }
+}
