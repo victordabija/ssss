46e81aecb50c0b724733ac79e0d0e0362075c688
Dabija Victor <victorsvlogs31@gmail.com>
2025-05-07 12:13:39 +0300

    feat: telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
